094ae087b29aa0f2f58291232b705cbdb673eb71
S R <sharlu@Ss-MacBook.broadband>
2020-09-25 20:48:55 +0100

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+app.use(express.json())
 app.use(cors());
 
 const welcomeMessage = {
@@ -16,8 +16,24 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
+// app.get("/", function (request, response) {
+//   response.sendFile(__dirname + "/index.html");
+// });
+
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+  response.send("Welcome to iMessage");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", (request, response) => {
+  response.send(messages);
+})
+
+app.post("/messages", (request, response) => {
+  const newMessage = request.body;
+  messages.push(newMessage);
+  console.log(request.body);
+  response.send(newMessage);
+  
+}); 
+
+app.listen(3075, () => console.log("Your app is listening on port 3075"));
